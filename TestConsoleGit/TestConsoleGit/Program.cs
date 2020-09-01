da75d36de2d04f5dabb0594d12001b9f8ed00ea2
peter.i.british@gmail.com <peter.i.british@gmail.com>
2020-09-01 12:29:41 +0300

    Second commit

--- TestConsoleGit/TestConsoleGit/Program.cs
+++ TestConsoleGit/TestConsoleGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestConsoleGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello Git! Second Work Day!");
         }
     }
 }
